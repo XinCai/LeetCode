950c552a2de61b32d9183fa77dd8373c0b8b16f6
CaiXin <XinCai@users.noreply.github.com>
2017-12-04 13:44:50 +1100

    TreeNode data structure 二叉树
    Define 节点

---
+++ TreeNode.java
@@ -0,0 +1,18 @@
+package DataStructure;
+
+/**
+ * Created by KXC176 on 4/12/2017.
+ * 二叉树
+ *
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
